2307e9e65da443870e9816be66168bd430c65505
sarpadda@gmail.com <arsh.bhatti8@gmail.com>
2018-12-07 00:25:41 -0800

    Added .gitIgnore

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const contactsRoutes = require('./routes/contacts');
 
 mongoose.connect(
-    'mongodb://arshbhatti8:'+
+    'mongodb://arshbhatti8:' +
     process.env.MONGO_ATLAS_PW +
     '@cluster0-shard-00-00-ovbfq.mongodb.net:27017,cluster0-shard-00-01-ovbfq.mongodb.net:27017,' +
     'cluster0-shard-00-02-ovbfq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
@@ -18,15 +18,25 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
 
-if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
-    return res.status(200).json({});
-}
-next();
+    app.use((error, req, res, next) => {
+        res.status(error.status || 500);
+        res.json({
+            error: {
+                message: error.message
+            }
+        });
+
+    });
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
 });
 
-app.use('/contacts',contactsRoutes);
+app.use('/contacts', contactsRoutes);
 
-module.exports=app;
+module.exports = app;
